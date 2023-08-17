d70f4e16b7250d15dea0b74aefc1f7ef850f4853
Ilia Zubov <97738404+ThePugSummoner@users.noreply.github.com>
2023-08-17 21:25:14 +0300

    some comment deleted

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -14,7 +14,7 @@ def index(request):
     return render(request, 'index.html', {})
 
 class MenuItemsView(generics.ListCreateAPIView):
-    #permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
@@ -22,29 +22,6 @@ class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [permissions.IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
-
-
-
-#class MenuView(APIView):
-#    
-#    def get(self, request):
-#        items = Menu.objects.all()
-#        serializer = MenuSerializer(items, many=True)
-#        return Response(serializer.data)
-#    
-#    def post(self,request):
-#        serializer = MenuSerializer(data=request.data)
-#
-#        if serializer.is_valid():
-#            serializer.save()
-#            return Response({"status": "success", "data": serializer.data})
-        
-#class BookingView(APIView):
-#    
-#    def get(self, request):
-#        items = Booking.objects.all()
-#        serializer = BookingSerializer(items, many=True)
-#        return Response(serializer.data)
 
 class BookingViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
