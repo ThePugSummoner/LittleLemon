0898a5996e30beda56e161df7228789dac27358a
Ilia Zubov <97738404+ThePugSummoner@users.noreply.github.com>
2023-08-17 23:16:39 +0300

    authentication deleted for menu items

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -14,7 +14,6 @@ def index(request):
     return render(request, 'index.html', {})
 
 class MenuItemsView(generics.ListCreateAPIView):
-    permission_classes = [permissions.IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
